5d9bd51c5088cf84549a0ea5a9605b5cdb319f64
Mumtaza15 <ahmad.fatha1502@mail.ugm.ac.id>
2023-12-06 01:12:00 +0700

    Commit login & register view

--- app/src/main/java/com/example/baskaryaapp/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/baskaryaapp/ui/login/LoginActivity.kt
@@ -1,11 +1,12 @@
-package com.example.baskaryaapp
+package com.example.baskaryaapp.ui.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.baskaryaapp.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
